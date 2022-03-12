4b7c9d8a5cd9c2b319be9063db9a6114bcb4d060
georgivelikov-devsearch <gmvelikov1@gmail.com>
2022-03-12 16:20:54 +0200

    Initial commit

---
+++ devsearch-config-server/src/test/java/devsearch/configserver/DevsearchConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package devsearch.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsearchConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
